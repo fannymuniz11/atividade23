c0e8bde6b598752c0002ce9bc9979d996b6a9065
fannymuniz11 <munizfanny03@gmail.com>
2024-09-18 16:12:39 -0300

    atividade23

---
+++ atividade23.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba um número do usuário e exiba a tabuada desse número de 1 a 10.
+numero = int(input("Digite um número para ver a tabuada: "))
+print(f"Tabuada de {numero}:")
+for i in range(1, 11):
+     resultado = numero * i
+     print(f"{numero} x {i} = {resultado}")
